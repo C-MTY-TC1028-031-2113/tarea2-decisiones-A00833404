9775a450e8c327000dcd31d0e4d481cae231aa44
A00833404 <A00833404@itesm.mx>
2021-08-26 14:55:14 -0500

    ejercicio 2 260821

--- assignments/03BMI/src/exercise.py
+++ assignments/03BMI/src/exercise.py
@@ -1,6 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso = float(input("Peso en kg: "))
+    alt = float(input("Altura en m: "))
+    if alt > 0 and peso > 0:
+        ind = peso / alt**2
+        if ind<20:
+            print("PESO BAJO")
+        elif 20 <= ind < 25:
+            print("NORMAL")
+        elif 25 <= ind < 30:
+            print("SOBREPESO")
+        elif 30 <= ind < 40:
+            print("OBESIDAD")
+        else:
+            print("OBESIDAD MORBIDA")
+    else: 
+        print("Revisa tus datos, alguno de ellos es erróneo")
+
 
 if __name__=='__main__':
     main()
+
