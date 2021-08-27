79f68d3d959f6ae27192fdb4e730352fa0c1fae5
A00833404 <A00833404@itesm.mx>
2021-08-27 12:12:03 -0500

    ejercicio 2 270821

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,7 +3,33 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    
+    if x > y and x > z:
+        print(x)
+        if y > z:
+            print(y)
+            print(z)
+        else:
+            print(z)
+            print(y)
+    elif y > x and y > z:
+        print(y)
+        if x > z:
+            print(x)
+            print(z)
+        else:
+            print(z)
+            print(x)
+    elif z > x and z > y:
+        print(z)
+        if x > y:
+            print(x)
+            print(y)
+        else:
+            print(y)
+            print(x)
+    else:
+        print("Ingresa 3 números diferentes.")
+         
 
 
 if __name__=='__main__':
