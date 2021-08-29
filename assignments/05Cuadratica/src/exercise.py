39c9ded5c4fbfe6bb55842d1292669cef4f298f2
A00833404 <A00833404@itesm.mx>
2021-08-29 18:54:47 -0500

    ejercicio 5

--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -5,7 +5,24 @@ def main():
     a=int(input("Da el valor de a: "))
     b=int(input("Da el valor de b: "))
     c=int(input("Da el valor de c: "))
-    pass
+    if a == 0 and b == 0:
+        print("No tiene solucion")
+    elif a == 0 and b != 0:
+        x = (-c/b)
+        print(x)
+    elif a != 0 and b != 0:
+        dis = (b**2-4*a*c)
+        if 0 > dis:
+            print("Raices complejas")
+        elif dis == 0:
+            x = -b/(2*a)
+            print(x)
+        else:
+            print(((-b) + dis)/ (2*a))
+            print(((-b) - dis)/ (2*a))
+    else: 
+        x = 0
+
 
 if __name__ == '__main__':
     main()
